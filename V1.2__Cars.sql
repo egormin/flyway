6b4828e7b6c14f0809a10d5d377fcc15dd9a7462
egormin <neoman2014@gmail.com>
2017-11-26 13:41:47 +0300

    Create V1.2__Cars.sql

---
+++ V1.2__Cars.sql
@@ -0,0 +1,8 @@
+create table XXX(
+    id INT(11) AUTO_INCREMENT PRIMARY KEY, 
+    username VARCHAR(100), 
+    email VARCHAR(100), 
+    password VARCHAR(100), 
+    description VARCHAR(100), 
+    register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
